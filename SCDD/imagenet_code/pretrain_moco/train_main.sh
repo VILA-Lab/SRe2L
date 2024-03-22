bf044df589e58ab1af2f9ca405cbef879922d422
LittleHead0 <zhoumuxin1218@gmail.com>
2024-03-22 10:35:52 +0400

    changed

--- SCDD/imagenet_code/pretrain_moco/train_main.sh
+++ SCDD/imagenet_code/pretrain_moco/train_main.sh
@@ -1,8 +1,7 @@
-CUDA_VISIBLE_DEVICES=0,1 \
 python main_moco.py \
   -a resnet18 \
   --lr 0.03 \
   --batch-size 16 \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
-  /data/ironman/zeyuan/imagenet \
+  /your/path/imagenet \
   --mlp --moco-t 0.2 --aug-plus --cos 
